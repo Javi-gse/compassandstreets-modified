885344967f2734fba7425a86e6d1aff19fe504cd
Javi-gse <72070422+Javi-gse@users.noreply.github.com>
2021-03-15 23:31:18 +0100

    Add files via upload

---
+++ compasandstreet/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'desconocido (Modificado por Javiツ#6046 y ParteMozas#7777)'
+description "MsQuerade's Compass and Streetname HUD (Adaptado para todas las resoluciones)"
+version "1.0"
+
+client_scripts {
+	"essentials.lua",
+	"compass.lua",
+	"streetname.lua"
+}
+
+ui_page "html/index.html"
+
+files {
+    "html/index.html",
+    "html/Adventure.otf",
+    "html/compasandstreet.js",
+    "html/compasandstreet.css"
+}
